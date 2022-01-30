d8b91be2790a90332e48f37c465958ae72971f5c
nykfeng <nykfengcode@gmail.com>
2022-01-30 13:15:47 -0500

    Blackjack card and deck functionalities

--- Blackjack/index.js
+++ Blackjack/index.js
@@ -1,8 +1,38 @@
-  console.log("♠", "♥", "♣", "♦");
-  console.log(`${chalk.bgWhite("    ")}`);
-  console.log(`${chalk.bgWhite(chalk.black(" ♠K "))}`);
-  console.log(`${chalk.bgWhite("    ")}`);
+import chalk from "chalk";
+import inquirer from "inquirer";
+import { createSpinner } from "nanospinner";
 
-  console.log(`${chalk.bgRed("    ")}`);
-  console.log(`${chalk.bgRed(" ♥A ")}`);
-  console.log(`${chalk.bgRed("    ")}`);
+import Card from "./Card.js";
+import Deck from "./deck.js";
+
+// const jack = new Card("♠", "J");
+// console.log(jack.color);
+
+// console.log(jack.card);
+
+const deck = new Deck();
+
+
+
+
+console.log(`let's draw a card`);
+console.log(deck.drawACard());
+console.log(deck.cards);
+console.log(deck.cards.length);
+
+// console.log("♠", "♥", "♣", "♦");
+// console.log(`${chalk.bgWhite("    ")}`);
+// console.log(`${chalk.bgWhite(chalk.black(" ♠K "))}`);
+// console.log(`${chalk.bgWhite("    ")}`);
+
+// console.log(`${chalk.bgRed("    ")}`);
+// console.log(`${chalk.bgRed(" ♥A ")}`);
+// console.log(`${chalk.bgRed("    ")}`);
+
+// const arr = [31, 59, 5, 11, 34, 321, 532, 324, 8, 102];
+
+// console.log(arr);
+
+// console.log(arr.pop());
+
+// console.log(arr);
